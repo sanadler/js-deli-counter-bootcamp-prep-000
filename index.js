b564b2f65396817c67d93cc26be4828cf7c64899
Sarah Nadler <sarah.m.nadler@gmail.com>
2017-11-10 03:03:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
-  var toReturn = "The line is currently: ";
   if (katzDeliLine.length === 0){
     return "The line is currently " +  "empty.";
   }
